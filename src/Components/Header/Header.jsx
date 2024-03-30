9a2ccb985c966fae30dff4ee9a534811c456a48b
YasharAzizov <yasharhere@gmail.com>
2024-03-30 18:40:45 +0400

    Restaurant Tomato

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Header/Header.css'
+import { assets } from "../../assets/assets";
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae consectetur nihil et minus non accusamus explicabo  debitis, itaque cupiditate? Itaque, quae omnis.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
